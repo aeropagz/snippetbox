ea7d83514d01cdf108e7dd85643ed9b8d2b00e16
aeropagz <klaas.pelzer@gmx.de>
2023-08-29 16:46:28 +0200

    dunno where i was

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -49,6 +49,9 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
+
+	data := &templateData{Snippet: s}
+
 	files := []string{
 		"./ui/html/show.page.tmpl",
 		"./ui/html/base.layout.tmpl",
@@ -61,7 +64,7 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = ts.Execute(w, s)
+	err = ts.Execute(w, data)
 	if err != nil {
 		app.serverError(w, err)
 	}
