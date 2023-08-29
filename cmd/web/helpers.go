aaeda3c42691331b37934bdc4aef68820cecb7f6
aeropagz <klaas.pelzer@gmx.de>
2023-08-29 22:08:01 +0200

    addd tmpl funcs

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"time"
 	"net/http"
 	"runtime/debug"
 )
@@ -28,8 +30,21 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, name stri
 		return
 	}
 
-	err := ts.Execute(w, td)
+	buf := new(bytes.Buffer)
+
+	err := ts.Execute(buf, app.addDefaultData(td, r))
 	if err != nil {
 		app.serverError(w, err)
+		return
 	}
+
+	buf.WriteTo(w)
+}
+
+func (app *application) addDefaultData(td *templateData, r *http.Request) *templateData {
+	if td == nil {
+		td = &templateData{}
+	}
+	td.CurrentYear = time.Now().Year()
+	return td
 }
