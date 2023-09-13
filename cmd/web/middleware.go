1ad08d123acbd1c4ec6454ac43af70ae21d20050
aeropagz <klaas.pelzer@gmx.de>
2023-09-13 16:00:12 +0300

    added context to r

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -3,7 +3,11 @@ package main
 import (
 	"fmt"
 	"net/http"
-	
+	"errors"
+	"context"
+	"os/user"
+
+	"github.com/aeropagz/snippetbox/pkg/models"
 	"github.com/justinas/nosurf"
 )
 
@@ -56,4 +60,26 @@ func noSurf(next http.Handler) http.Handler {
 		Secure: true,
 	})
 	return csrfHandler
+}
+
+func (app *application) authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		exists := app.session.Exists(r, "authenticatedUserID")
+		if !exists {
+			next.ServeHTTP(w, r)
+			return
+		} 
+		user, err := app.users.Get(app.session.GetInt(r, "authenticatedUserID"))
+
+		if errors.Is(err, models.ErrNoRecord) || !user.Active {
+			app.session.Remove(r, "authenticatedUserID")
+			next.ServeHTTP(w, r)
+			return
+		} else if err != nil {
+			app.serverError(w, err)
+			return
+		}
+		ctx := context.WithValue(r.Context(), contextKeyIsAuthenticated, true)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
 }
